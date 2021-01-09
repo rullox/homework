876d2e6b1693f66b8b46ef4d520259c7149f40dd
rullox <56354979+rullox@users.noreply.github.com>
2021-01-09 17:16:05 +0800

    Update and rename msg_receive.go to sql_msg_receive.go

--- Go/sql_msg_receive.go
+++ Go/sql_msg_receive.go
@@ -15,6 +15,8 @@ type SqlMessage struct {
 	UserID    int		//用户ID
 	GoodsID    int		//商品ID
 	Count    int		//抢购商品数量
+	GoodsPrice    int		//商品价格
+            	Number    string		//订单号
 }
 
 //goods ...
@@ -23,6 +25,8 @@ type Goods struct {
 	Name string `gorm:"column:name"`
 	Count   int    `gorm:"column:count"`
 	Status    int    `gorm:"column:status"`    //0 非卖品
+	Pic   string    `gorm:"column:pic"`
+	Price   int    `gorm:"column:price"`
 }
 
 //user ...
@@ -31,15 +35,17 @@ type User struct {
 	Username	    string		`gorm:"column:username"`	
 	Password    string		`gorm:"column:password"`	
 	Token    string		`gorm:"column:token"`
+	Money    int		`gorm:"column:money"`
 }
 
-//log....
-type Log struct {
-	ID     int			`gorm:"primaryKey"`
+//list....
+type List struct {
+	//ID     int			`gorm:"primaryKey"`
 	User_id    int		`gorm:"column:user_id"`	
 	User_name    string		`gorm:"column:user_name"`	
 	Goods_id    int		`gorm:"column:goods_id"`
 	Goods_name    string	`gorm:"column:goods_name"`
+	Number    string		`gorm:"column:number"`
 }
 
 func failOnError(err error, msg string) {
@@ -48,9 +54,9 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func choose(user_id int, goods_id int, count int) string{
-	//连接数据库
+func choose(user_id int, goods_id int, goods_price int, count int, number string) string{
 
+	//连接数据库
 	dsn := "root:hwj@tcp(127.0.0.1:3306)/homework?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -59,17 +65,17 @@ func choose(user_id int, goods_id int, count int) string{
 	fmt.Println("------------连接数据库成功-----------")
 	isok := 0
 
-	// 取出user
+	// 取出user，查看客户的余额是否足够
 	user := User{}
-	errUser := db.Table("user").Where("id = ?", user_id).First(&user).Error
+	errUser := db.Table("users").Where("id = ?", user_id).Where("money >= ?", goods_price).First(&user).Error
 	if errors.Is(errUser, gorm.ErrRecordNotFound) {
-		return ("该用户不满足购买条件！")
+		return ("购买失败！请重试！")
 	} else {
 		isok = 1
 		fmt.Println(user.Username)
 	}
 
-	// 商品是否有存货，不够则失败
+	// 商品是否有存货
 	goods := Goods{}
 	errGoods := db.Table("goods").Where("status = 1").Where("id = ?", goods_id).Where("count >= ?", count).First(&goods).Error
 	if errors.Is(errGoods, gorm.ErrRecordNotFound) { 
@@ -83,6 +89,24 @@ func choose(user_id int, goods_id int, count int) string{
 			tx.Rollback()
 			fmt.Println(err)
 			return "购买失败！请重试！"
+		}
+		err = tx.Table("users").Where("id = ?", user_id).Update("money", user.Money - goods_price).Error
+		if err != nil {
+			tx.Rollback()
+			fmt.Println(err)
+			return "购买失败！请重试！"
+		}
+		list := List{
+			User_id: user_id, 
+			User_name: user.Username, 
+			Goods_id: goods_id, 
+			Goods_name: goods.Name, 
+			Number: number,
+		}
+		err = tx.Table("list").Create(&list).Error
+		if err != nil {
+			tx.Rollback()
+			fmt.Println(err)
 		}
 	}
 
@@ -136,14 +160,14 @@ func main() {
 				return
 			}
 			fmt.Printf("mes:%#v\n", mes)
- 			res := choose(mes.UserID, mes.GoodsID, mes.Count)
+ 			res := choose(mes.UserID, mes.GoodsID, mes.GoodsPrice, mes.Count, mes.Number)
 			fmt.Printf("res:%s\n", res)
 		}
 	}()
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
 
+	<-forever
 
 
 }
